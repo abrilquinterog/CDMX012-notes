8e706894d56e73a992092d2c055ba2a6132c9634
abrilquinterog <abril_quintero@hotmail.com>
2022-06-03 20:52:36 -0500

    Añade funcionalidad de editar y actualixar nota

--- notes-app/src/components/noautenticate/Paths.js
+++ notes-app/src/components/noautenticate/Paths.js
@@ -2,12 +2,14 @@ import { HomeScreen } from './HomeScreen';
 import {Route, Routes} from 'react-router-dom';
 import { NoteScreen } from '../autenticate/NoteScreen';
 import { loginWithGoogle, logOut} from '../../lib/firebaseAuth';
+import { EditNote } from '../autenticate/EditNote';
 
 export const Paths = ({isAutenticate}) =>
  { 
     return (<div>{isAutenticate?
       <Routes>
       <Route path="/" element={<NoteScreen logOut={logOut}/>} />
+      <Route path="/edit/:id" element={<EditNote logOut={logOut}/>} />
       </Routes>:
       <Routes>
       <Route path="/" element={<HomeScreen loginWithGoogle={loginWithGoogle}/>} />
